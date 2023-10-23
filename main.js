6eba3e2bf1498b2fb23e8434bf7a5dd81a36ba6f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-10-23 16:18:08 -0300

    feat: create project

---
+++ main.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const fs = require("fs")
+const { execSync } = require("child_process")
+
+const rootDir = process.env.INPUT_ROOT
+const isTest = process.env.INPUT_TEST === "true"
+
+function getPackage() {
+    try {
+        const packagePath = path.resolve(rootDir, "package.json")
+        const packageContent = fs.readFileSync(packagePath)
+
+        if (!packageContent) {
+            throw new Error("No package.json content")
+        }
+
+        return JSON.parse(packageContent)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+const { version } = getPackage()
+
+if (isTest) {
+    console.log(version)
+    process.exit(1)
+}
+
+if (!isTest) {
+    execSync(`git tag v${version}`)
+    execSync(`git push origin --tags`)
+    process.exit(0)
+}
