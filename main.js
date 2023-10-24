01419fc16f6d095ce0b8b2dc666421e2880b51af
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-10-23 22:48:58 -0300

    feat: add tag exists validation

--- main.js
+++ main.js
@@ -5,36 +5,52 @@ const { execSync } = require("child_process")
 const rootDir = process.env.INPUT_ROOT
 const isTest = process.env.INPUT_TEST === "true"
 
-function getPackage() {
-    try {
-        const packagePath = path.resolve(rootDir, "package.json")
-        const packageContent = fs.readFileSync(packagePath)
 
-        if (!packageContent) {
-            throw new Error("No package.json content")
+const exitCode = main()
+process.exit(exitCode)
+
+
+function main() {
+    try {
+        const { version } = getPackage()
+
+        if (!version) {
+            throw new Error("No version provided in package.json")
         }
 
-        return JSON.parse(packageContent)
+        if (isTest) {
+            console.log(version)
+            return 0
+        }
+
+        const currentTags = execSync("git tag -l").toString().split("\n")
+
+        const tagName = `v${version}`
+
+        if (currentTags.includes(tagName)) {
+            console.log(`Current tag "${tagName}" already exists.`)
+            console.log("Aborting......")
+            return 0
+        }
+
+        if (!isTest) {
+            execSync(`git tag ${tagName}`)
+            execSync(`git push origin --tags`)
+            return 0
+        }
     } catch (error) {
         console.error(error)
-        process.exit(1)
+        return 1
     }
 }
 
-const { version } = getPackage()
+function getPackage() {
+    const packagePath = path.resolve(rootDir, "package.json")
+    const packageContent = fs.readFileSync(packagePath)
 
-if (!version) {
-    console.error("No version provided in package.json")
-    process.exit(1)
-}
+    if (!packageContent) {
+        throw new Error("No package.json content")
+    }
 
-if (isTest) {
-    console.log(version)
-    process.exit(0)
-}
-
-if (!isTest) {
-    execSync(`git tag v${version}`)
-    execSync(`git push origin --tags`)
-    process.exit(0)
+    return JSON.parse(packageContent)
 }
